a371e90dec0237741fd202ce85809f71e0a9581d
Jack <superjackpower85@gmail.com>
2019-10-29 00:29:33 +0800

    Integrated technologies seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,3 +50,11 @@ puts "8 blog posts created"
     )
 end
 puts "1 Portfolio items created"
+
+3.times do |technology|
+    Portfolio.last.technologies.create!(
+        name: "Technology #{technology}",
+    )
+end
+
+puts "3 Technologies items created"
