4ab66550e70b54a772cba0b34b2ddac31b1f2473
Jack <superjackpower85@gmail.com>
2019-11-18 23:28:06 +0800

    Implemented styles for the inital blog layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
         content_tag(:div, "My Content", class: "my-class")
     end
 
-    def login_helper style
+    def login_helper style = ''
         if current_user.is_a?(GuestUser) 
             (link_to "Sign Up", new_user_registration_path, class: style) + " ".html_safe + 
             (link_to "Login", new_user_session_path, class: style)
