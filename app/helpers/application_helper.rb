98d3c37bcba382c74b98e20a99fb696b225ad360
Jack <superjackpower85@gmail.com>
2019-11-25 14:35:14 +0800

    implemented refactor for nav and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,9 +24,44 @@ module ApplicationHelper
         JackViewTool::Renderer.copyright 'Jack First', 'All rights reserved.'
     end
 
-#     def nav_helper style, tag_type
-# nav_links = <<NAV
-# <#{tag_type}><a href="#{root_path}" class="#{style}"> Home</a></#{tag_type}>
-
+#     def nav_items
+#         [
+#             {
+#                 url: root_path
+#                 title: 'Home'
+#             },
+#             {
+#                 url: about_me_path
+#                 title: 'About Me'
+#             },
+#             {
+#                 url: contact_path
+#                 title: 'Contact'
+#             },
+#             {
+#                 url: blogs_path
+#                 title: 'Blog'
+#             },
+#             {
+#                 url: portfolios_path
+#                 title: 'Portfolio'
+#             }
+#         ]
 #     end
+
+#     def nav_helper style, tag_type
+#         nav_links = ''
+
+#         nav_items.each do |item|
+#             nav_links << "#{tag_type}><a href='#{item[:url]} class='#{style}' #{active? item[:url]}'> #{item[:titel]}</a></#{tag_type}>"
+# # nav_links = <<NAV
+# # <#{tag_type}><a href="#{root_path}" class="#{style}"> Home</a></#{tag_type}>
+# # <#{tag_type}><a href="#{about_me_path}" class="#{style}"> About Me</a></#{tag_type}>
+# # NAV
+#         nav_links.html_safe
+#     end
+
+    def active? path 
+        "active" if current_page? path 
+    end
 end
