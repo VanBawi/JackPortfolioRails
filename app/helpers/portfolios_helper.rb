23b44bc53f5ca303231d5549ed5dae7a52cb6931
Jack Van <superjackpower85@gmail.com>
2020-02-25 01:09:34 +0800

    refactored the placeholder for carrier wave

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,20 @@
 module PortfoliosHelper
+    def image_generator(height:, width:)
+        "https://placehold.it/#{height}x#{width}"
+    end
+
+    def portfolio_img img, type
+        if img
+            img
+        elsif type == 'thumb'
+            image_generator(height: '350', width: '200')
+        else
+            image_generator(height: '600', width: '400')
+        end
+    end
+
 end
+
+
+# self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+# self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
