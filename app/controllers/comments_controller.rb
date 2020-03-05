61c94d63f5b4b6182ede84aa2b4e6b5dc3a8b245
Jack Van <superjackpower85@gmail.com>
2020-03-05 17:12:51 +0800

    implemented new comment content funtion

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+
+    def create
+        @comment = current_user.comment.build(comment_params)
+    end
+
+    private
+    
+    def comment_params
+        param.require(:comment).permit(:content)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,14 +17,14 @@ class User < ApplicationRecord
 
   has_many :comments, dependent: :destroy
 
-  
+
   def first_name
       self.name.split.first
     
   end
 
   def last_name
-    self.name.split.last
+  self.name.split.last 
   end
 end
 
